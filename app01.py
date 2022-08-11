3f4bb4d3b1705717bac681e37251137f919e1855
Kuniyoshi Nakane <nc2k-nkn@asahi-net.or.jp>
2022-08-12 02:06:45 +0900

    2022.08.12.ver1.0

--- app01.py
+++ app01.py
@@ -1,41 +1,67 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import japanize_matplotlib
 from st_aggrid import AgGrid
 
 st.set_page_config(layout="wide")
 
-data_url = 'https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv'
+data_url_newly_confirmed = 'https://covid19.mhlw.go.jp/public/opendata/newly_confirmed_cases_daily.csv'
+data_url_severe_cases = 'https://covid19.mhlw.go.jp/public/opendata/severe_cases_daily.csv'
 
-st.title('ガジェット研究会　新型コロナ関連ダッシュボード')
-st.caption('新型コロナに関連したデータです')
-
-st.text('新規感染者データ')
+st.title('新型コロナ関連ダッシュボード（ガジェット研究会）')
+st.caption('新型コロナ関連データです')
 
 col1, col2 = st.columns(2)
 
-df = pd.read_csv(data_url)
-df['Datetime'] = pd.to_datetime(df['Date'])
-df_tochigi = df[['Datetime', 'Tochigi', 'ALL']].copy()
+df_nc = pd.read_csv(data_url_newly_confirmed)
+df_nc['Datetime'] = pd.to_datetime(df_nc['Date'])
+df_nc_tochigi = df_nc[['Datetime', 'Tochigi', 'ALL']].copy()
+
+df_sc = pd.read_csv(data_url_severe_cases)
+df_sc['Datetime'] = pd.to_datetime(df_sc['Date'])
+df_sc_tochigi = df_sc[['Datetime', 'Tochigi', 'ALL']].copy()
 
 with col1:
-    AgGrid(df_tochigi,theme="blue",fit_columns_on_grid_load=True)
+    st.text('新規感染者')
+    AgGrid(df_nc_tochigi,theme="blue")
+
+    st.text('重症者')
+    AgGrid(df_sc_tochigi,theme="blue")
+
+
 
 with col2:
 
-    #matplotlib
-    fig = plt.figure(figsize=(12,6))
+    #新規感染者
+    fig = plt.figure(figsize=(12,5))
 
     ax1 = fig.add_subplot(1, 2, 1)
     plt.xticks(rotation=45)
     ax2 = fig.add_subplot(1, 2, 2)
     plt.xticks(rotation=45)
 
-    ax1.plot(df['Datetime'], df['Tochigi'])
-    ax1.set_title('Tochigi')
+    ax1.bar(df_nc['Datetime'], df_nc['Tochigi'])
+    ax1.set_title('新規感染者　栃木')
 
-    ax2.plot(df['Datetime'], df['ALL'])
-    ax2.set_title('ALL')
+    ax2.bar(df_nc['Datetime'], df_nc['ALL'])
+    ax2.set_title('新規感染者　全国')
+
+    st.pyplot(fig)
+
+    #重症者
+    fig = plt.figure(figsize=(12,5))
+
+    ax1 = fig.add_subplot(1, 2, 1)
+    plt.xticks(rotation=45)
+    ax2 = fig.add_subplot(1, 2, 2)
+    plt.xticks(rotation=45)
+
+    ax1.bar(df_sc['Datetime'], df_sc['Tochigi'])
+    ax1.set_title('重症者　栃木')
+
+    ax2.bar(df_sc['Datetime'], df_sc['ALL'])
+    ax2.set_title('重症者　全国')
 
     st.pyplot(fig)
 
