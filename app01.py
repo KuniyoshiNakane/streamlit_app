412ddd18f90b08d114bb70f40c5d03d7dfb5916b
Kuniyoshi Nakane <nc2k-nkn@asahi-net.or.jp>
2022-08-30 18:42:15 +0900

    Update app01.py

--- app01.py
+++ app01.py
@@ -38,13 +38,13 @@ df_nc_sc_tochigi['重症者_Tochigi'].fillna(0, inplace=True)
 
 with col1:
     st.text('新規感染者')
-    AgGrid(df_nc_tochigi, theme="blue")
+    AgGrid(df_nc_tochigi)
 
     st.text('重症者')
-    AgGrid(df_sc_tochigi, theme="blue")
+    AgGrid(df_sc_tochigi)
 
     st.text('PCR検査実施人数')
-    AgGrid(df_pcr, theme='blue')
+    AgGrid(df_pcr)
 
 
 
